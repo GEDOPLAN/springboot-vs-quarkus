f6b908e68a12bd7051991b747e017a17daf9fc1e
Dirk Weil <dirk.weil@gedoplan.de>
2024-05-14 21:50:34 +0200

    Fix test command syntax

--- scripts/wait_for_response.sh
+++ scripts/wait_for_response.sh
@@ -2,4 +2,4 @@
 # - run this script
 # - start the server application with date +"%T.%3N" && java -jar target/...
 # - Compare the timestamp of the debug log message of GreetingResource with the initial timestamp on the server console
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/api/greeting)" != "200" ]]; do sleep .00001; done
+while [ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/api/greeting)" != "200" ]; do sleep .00001; done
